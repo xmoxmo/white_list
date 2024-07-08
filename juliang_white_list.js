9f5fc514b40787e02dc317c15511bd866e85d43c
xmo <henwuji007@163.com>
2024-07-08 14:36:04 +0800

    Update juliang_white_list.js

--- juliang_white_list.js
+++ juliang_white_list.js
@@ -180,36 +180,36 @@ function wxpusherNotify(text, desp, strsummary = "") {
             //desp = `<font size="3">${desp}</font>`;
             desp = desp.replace(/[\n\r]/g, '<br>'); // 默认为html, 不支持plaintext
             desp = `<section style="width: 24rem; max-width: 100%;border:none;border-style:none;margin:2.5rem auto;" id="shifu_imi_57"
-    donone="shifuMouseDownPayStyle(&#39;shifu_imi_57&#39;)">
-    <section
-        style="margin: 0px auto;text-align: left;border: 2px solid #212122;padding: 10px 0px;box-sizing:border-box; width: 100%; display:inline-block;"
-        class="ipaiban-bc">
-        <section style="margin-top: 1rem; float: left; margin-left: 1rem; margin-left: 1rem; font-size: 1.3rem; font-weight: bold;">
-            <p style="margin: 0; color: black">
-                ${text}
-            </p>
-        </section>
-        <section style="display: block;width: 0;height: 0;clear: both;"></section>
-        <section
-            style="margin-top:20px; display: inline-block; border-bottom: 1px solid #212122; padding: 4px 20px; box-sizing:border-box;"
-            class="ipaiban-bbc">
-            <section
-                style="width:25px; height:25px; border-radius:50%; background-color:#212122;display:inline-block;line-height: 25px"
-                class="ipaiban-bg">
-                <p style="text-align:center;font-weight:1000;margin:0">
-                    <span style="color: #ffffff;font-size:20px;">📢</span>
-                </p>
-            </section>
-            <section style="display:inline-block;padding-left:10px;vertical-align: top;box-sizing:border-box;">
-            </section>
-        </section>
-        <section style="margin-top:0rem;padding: 0.8rem;box-sizing:border-box;">
-            <p style=" line-height: 1.6rem; font-size: 1.1rem; ">
-                ${desp} 
-			</p>            
-        </section>
-    </section>
-</section>`;
+					donone="shifuMouseDownPayStyle(&#39;shifu_imi_57&#39;)">
+					<section
+						style="margin: 0px auto;text-align: left;border: 2px solid #212122;padding: 10px 0px;box-sizing:border-box; width: 100%; display:inline-block;"
+						class="ipaiban-bc">
+						<section style="margin-top: 1rem; float: left; margin-left: 1rem; margin-left: 1rem; font-size: 1.3rem; font-weight: bold;">
+							<p style="margin: 0; color: black">
+								${text}
+							</p>
+						</section>
+						<section style="display: block;width: 0;height: 0;clear: both;"></section>
+						<section
+							style="margin-top:20px; display: inline-block; border-bottom: 1px solid #212122; padding: 4px 20px; box-sizing:border-box;"
+							class="ipaiban-bbc">
+							<section
+								style="width:25px; height:25px; border-radius:50%; background-color:#212122;display:inline-block;line-height: 25px"
+								class="ipaiban-bg">
+								<p style="text-align:center;font-weight:1000;margin:0">
+									<span style="color: #ffffff;font-size:20px;">📢</span>
+								</p>
+							</section>
+							<section style="display:inline-block;padding-left:10px;vertical-align: top;box-sizing:border-box;">
+							</section>
+						</section>
+						<section style="margin-top:0rem;padding: 0.8rem;box-sizing:border-box;">
+							<p style=" line-height: 1.6rem; font-size: 1.1rem; ">
+								${desp} 
+							</p>            
+						</section>
+					</section>
+				</section>`;
 
             const body = {
                 appToken: `${WP_APP_TOKEN_ONE}`,
