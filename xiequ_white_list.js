c3587b4e01cc461847586015369d3a0d999b740f
xmo <henwuji007@163.com>
2024-07-09 14:30:03 +0800

    Update xiequ_white_list.js

--- xiequ_white_list.js
+++ xiequ_white_list.js
@@ -135,7 +135,7 @@ async function delwhiteip(oldip) {
         }
     });
   });
-  console.log('💡 白名单删除失效IP的响应:', delIpResponse.body);
+  console.log('💡 白名单中删除上次IP:' + oldip + ',', delIpResponse.body);
   return delIpResponse.body;
 }
 
