e0003c59ee4fb8cbc389a21440ca596dd5fa24f4
xmo <112841961+xmoxmo@users.noreply.github.com>
2024-10-17 03:26:00 +0800

    Update sendNotify.js

--- sendNotify.js
+++ sendNotify.js
@@ -21,7 +21,7 @@ const querystring = require('querystring');
 const exec = require('child_process').exec;
 const $ = new Env();
 const timeout = 15000; //超时时间(单位毫秒)
-console.log("   *加载sendNotify，当前版本: 20230712[20240715]+fs202410151635");
+console.log("   *加载sendNotify，当前版本: 20240612[202410170320]");
 // =======================================go-cqhttp通知设置区域===========================================
 //gobot_url 填写请求地址http://127.0.0.1/send_private_msg
 //gobot_token 填写在go-cqhttp文件设置的访问密钥
@@ -195,7 +195,7 @@ if (process.env.NOTIFY_SHOWNAMETYPE) {
     if (ShowRemarkType == "4")
         console.log("检测到显示备注名称，格式为: 备注");
 }
-async function sendNotify(text, desp, params = {}, author = `\n\n本通知 By ${author}`,strsummary="") {
+async function sendNotify(text, desp, params = {}, author = `\n\n本通知 By xmo`,strsummary="") {
     console.log(`开始发送通知...`);
 
     //NOTIFY_FILTERBYFILE代码来自Ca11back.
@@ -985,7 +985,7 @@ function getRemark(strRemark) {
     }
 }
 
-async function sendNotifybyWxPucher(text, desp, PtPin, author = `\n\n本通知 By ${author}`, strsummary = "") {
+async function sendNotifybyWxPucher(text, desp, PtPin, author = `\n\n本通知 By xmo`, strsummary = "") {
 
     try {
         var Uid = "";
